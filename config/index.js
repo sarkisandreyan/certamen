acb3301254908346e8a79b3c1adb43a1b57d8b04
Narek <Narek11harutyunyan@gmail.com>
2017-10-20 13:32:55 +0400

    init

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const config = {
+  port: process.env.PORT || 3000,
+  db: 'mongodb://localhost:27017/mydb'
+};
+
+export default config;
---
+++ public/src/app.js
@@ -0,0 +1,4 @@
+angular.module('myApp', []).controller('MyCtrl', function($scope){
+  var vm = this;
+  vm.firstName = 'Heloo, this is from angular controller'
+})
